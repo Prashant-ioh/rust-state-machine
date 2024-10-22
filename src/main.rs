8a791ec7338d9a05c7ec3ae0db0d978be4a05329
Prashant-ioh <vikky4424@gmail.com>
2024-10-22 21:45:37 +0530

    rust-state-machine

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ mod balances;
 
 fn main() {
     println!("Hello, Prashant mishra!");
-    println!("hello rust dev!");
-    println!("hello rust strrate!");
-    println!("hello rust machine!");
-
 }
 
 
